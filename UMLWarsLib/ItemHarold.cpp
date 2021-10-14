f77f55ebe06cf09a263331df6618bacb269f5a98
Hemkesh Agrawal <agrawalh@msu.edu>
2021-10-14 17:22:56 -0400

    Fixed the radio buttons for variant

---
+++ UMLWarsLib/ItemHarold.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file ItemHarold.cpp
+ * @author hemke
+ */
+
+#include "ItemHarold.h"
+
+using namespace std;
+
+/// Harold filename
+const wstring HaroldImageName = L"images/harold.png";
+
+/**
+ * Constructor
+ * @param umlwars Aquarium this fish is a member of
+ */
+ItemHarold::ItemHarold(UMLWars *umlwars) : Item(umlwars, HaroldImageName)
+{
+
+}
---
+++ UMLWarsLib/ItemHarold.h
@@ -0,0 +1,28 @@
+/**
+ * @file ItemHarold.h
+ * @author hemke
+ *
+ *
+ */
+
+#ifndef UMLWARS_ITEMHAROLD_H
+#define UMLWARS_ITEMHAROLD_H
+
+#include "Item.h"
+
+class ItemHarold : public Item{
+public:
+    /// Default constructor (disabled)
+    ItemHarold() = delete;
+
+    /// Copy constructor (disabled)
+    ItemHarold(const ItemHarold &) = delete;
+
+    /// Assignment operator
+    void operator=(const ItemHarold &) = delete;
+
+    ItemHarold(UMLWars* umlwars);
+
+};
+
+#endif //UMLWARS_ITEMHAROLD_H
